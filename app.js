9c87a76374a592b2f3ed2c4245c40cf9c7046146
Ziyu <songziyu98@gmail.com>
2021-04-25 15:24:53 +0800

    error handling during bad upload file

--- app.js
+++ app.js
@@ -14,12 +14,14 @@ const princeOptions = ' --pdf-profile=\"PDF/UA-1\" -o ';
 
 // config random filename generator
 const FILENAME_LENGTH = 21;
-const Crypto = require('crypto');
-function randomString(size = FILENAME_LENGTH) {  
-  return Crypto
-    .randomBytes(size)
-    .toString('base64')
-    .slice(0, size)
+function randomString(length = FILENAME_LENGTH) {
+    var result           = [];
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result.push(characters.charAt(Math.floor(Math.random() * charactersLength)));
+    }
+    return result.join('');
 }
 
 // config MathJax
@@ -74,66 +76,76 @@ app.post('/api/LaTexUpload', (req, res, next) => {
 
     // upload finishes
     form.on('end', () => {
-        const LaTexPath = ' ./temp/' + filename + '.tex ';
-        const HTMLPath = ' ./temp/' + filename + '.html ';
-        execSync('pandoc' + LaTexPath + pandocOptions + HTMLPath, (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-        });
+        try {
+            const LaTexPath = ' ./temp/' + filename + '.tex ';
+            const HTMLPath = ' ./temp/' + filename + '.html ';
+            execSync('pandoc' + LaTexPath + pandocOptions + HTMLPath, (error, stdout, stderr) => {
+                if (error) {
+                    console.log(`error: ${error.message}`);
+                    return;
+                }
+                if (stderr) {
+                    console.log(`stderr: ${stderr}`);
+                    return;
+                }
+                console.log(`stdout: ${stdout}`);
+            });
 
-        const HTMLGlobalPath = __dirname + '/temp/' + filename + '.html';
-        var data = fs.readFileSync(HTMLGlobalPath, 'utf8');
-        // adding MathJax to HTML
-        var alttext = "Alternative Text Goes Here"
-        var idx = data.indexOf('<math');
-        while (idx > 0) {
-            var math_end = data.indexOf('>', idx);
-            var ml_start = data.indexOf('>', data.indexOf(SemTag,math_end))+1;
-            var ml_end = data.indexOf(AnnTag,ml_start);
-            var ml_struct = data.slice(ml_start, ml_end);
-            alttext = sre.toSpeech(ml_struct);
-            data = data.slice(0,math_end) + " " + AltTextAttr + "\"" + alttext + "\""
-                 + data.slice(math_end)
-            idx = data.indexOf('<math', idx+1);
+            const HTMLGlobalPath = __dirname + '/temp/' + filename + '.html';
+            var data = fs.readFileSync(HTMLGlobalPath, 'utf8');
+            // adding MathJax to HTML
+            var alttext = "Alternative Text Goes Here"
+            var idx = data.indexOf('<math');
+            while (idx > 0) {
+                var math_end = data.indexOf('>', idx);
+                var ml_start = data.indexOf('>', data.indexOf(SemTag,math_end))+1;
+                var ml_end = data.indexOf(AnnTag,ml_start);
+                var ml_struct = data.slice(ml_start, ml_end);
+                alttext = sre.toSpeech(ml_struct);
+                data = data.slice(0,math_end) + " " + AltTextAttr + "\"" + alttext + "\""
+                    + data.slice(math_end)
+                idx = data.indexOf('<math', idx+1);
+            }
+            var dataWithMath = 
+                '<!DOCTYPE html><html lang=\'en\'><head><title>Converted-PDF</title></head><body><h1></h1><h2></h2><h3></h3>\n' 
+                + data 
+                + '</body></html>';
+            // finish adding MathJax to HTML
+            fs.writeFileSync(HTMLGlobalPath, dataWithMath, 'utf8', function (err) {
+                if (err) return console.log(err);
+            });
+
+            // prince prince1.html --pdf-profile="PDF/UA-1"
+            const PDFPath = __dirname + '/temp/' + filename + '.pdf';
+            execSync('prince' + HTMLPath + princeOptions + PDFPath, (error, stdout, stderr) => {
+                if (error) {
+                    console.log(`error: ${error.message}`);
+                    return;
+                }
+                if (stderr) {
+                    console.log(`stderr: ${stderr}`);
+                    return;
+                }
+                console.log(`stdout: ${stdout}`);
+            });
+
+            res.download(PDFPath, 'result.pdf');
+
+            // cleanup
+            res.on('finish', function() {  
+                removeFile(filename + '.tex');
+                removeFile(filename + '.html');
+                removeFile(filename + '.pdf');
+            });
+        } catch (err) {
+            res.sendFile(__dirname + '/public/bad.html');
+            res.on('finish', function() {  
+                removeFile(filename + '.tex');
+                removeFile(filename + '.html');
+                removeFile(filename + '.pdf');
+            });
         }
-        var dataWithMath = 
-            '<!DOCTYPE html><html lang=\'en\'><head><title>Converted-PDF</title></head><body><h1></h1><h2></h2><h3></h3>\n' 
-            + data 
-            + '</body></html>';
-        // finish adding MathJax to HTML
-        fs.writeFileSync(HTMLGlobalPath, dataWithMath, 'utf8', function (err) {
-            if (err) return console.log(err);
-        });
-
-        // prince prince1.html --pdf-profile="PDF/UA-1"
-        const PDFPath = __dirname + '/temp/' + filename + '.pdf';
-        execSync('prince' + HTMLPath + princeOptions + PDFPath, (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-        });
-
-        res.download(PDFPath, 'result.pdf');
-
-        // cleanup
-        res.on('finish', function() {  
-            removeFile(filename + '.tex');
-            removeFile(filename + '.html');
-            removeFile(filename + '.pdf');
-        });
+        
     });
 });
 
